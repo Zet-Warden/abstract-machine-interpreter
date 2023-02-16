ae1ade88209424fb675edab88a7bfa26251ee46b
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2023-02-16 19:38:06 +0800

    added tests for Tape clone method

--- tests/lib/memory/Tape.test.ts
+++ tests/lib/memory/Tape.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, test } from '@jest/globals';
 import dedent from 'ts-dedent';
 import Tape from '../../../lib/memory/Tape';
+import TapeCell from '../../../lib/memory/TapeCell';
+
+//Ø
 
 describe('Memory Tape', () => {
     describe('Moving the tape', () => {
@@ -75,6 +78,49 @@ describe('Memory Tape', () => {
             const tape = new Tape('1100');
             expect(tape.toString()).toBe('1100');
         });
+    });
+
+    describe('Deep clone of Tape memory', () => {
+        const tape = new Tape('10010');
+        tape.moveRight();
+
+        const newTape = tape.clone();
+
+        expect(newTape.currentSymbol).toBe('0');
+
+        newTape.moveRight();
+        newTape.moveRight();
+
+        expect(newTape.currentSymbol).toBe('1');
+        expect(tape.currentSymbol).toBe('0');
+
+        newTape.moveDown();
+
+        const newerTape = newTape.clone();
+        newerTape.moveUp();
+        newerTape.write('A');
+
+        newTape.moveDown();
+        tape.moveUp();
+        tape.moveUp();
+        tape.moveUp();
+
+        expect(tape.toString()).toBe(dedent`
+        ØØØØØ
+        ØØØØØ
+        ØØØØØ
+        10010`);
+
+        expect(newTape.toString()).toBe(dedent`
+        10010
+        ØØØØØ
+        ØØØØØ`);
+
+        expect(newerTape.toString()).toBe(dedent`
+        100A0
+        ØØØØØ`);
+
+        expect(newerTape.currentSymbol).toBe('A');
     });
 });
 
