55f8439aa05342a3b77bff500165340563bdc4ce
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2023-02-16 15:33:01 +0800

    Create TapeCell.ts

---
+++ lib/memory/TapeCell.ts
@@ -0,0 +1,68 @@
+import Directions from '../utils/Direction';
+
+export default class TapeCell {
+    private _symbol: string;
+    left: TapeCell;
+    right: TapeCell;
+    up: TapeCell;
+    down: TapeCell;
+
+    static BLANK_SYMBOL: string = 'Ø';
+
+    constructor(symbol: string = TapeCell.BLANK_SYMBOL) {
+        this._symbol = symbol;
+    }
+
+    get symbol() {
+        return this._symbol;
+    }
+
+    join(tapeBlock: TapeCell, direction: Directions) {
+        switch (direction) {
+            case Directions.LEFT:
+                this.joinLeft(tapeBlock);
+                break;
+            case Directions.RIGHT:
+                this.joinRight(tapeBlock);
+                break;
+            case Directions.UP:
+                this.joinUp(tapeBlock);
+                break;
+            case Directions.DOWN:
+                this.joinDown(tapeBlock);
+                break;
+            default:
+                throw new Error(
+                    `Umatched case ${direction} for direction. Expecting "L", "R", "U", "D".`
+                );
+        }
+    }
+
+    write(symbol: string) {
+        this._symbol = symbol;
+    }
+
+    joinLeft(tapeBlock: TapeCell) {
+        this.left = tapeBlock;
+        tapeBlock.right = this;
+    }
+
+    joinRight(tapeBlock: TapeCell) {
+        this.right = tapeBlock;
+        tapeBlock.left = this;
+    }
+
+    joinUp(tapeBlock: TapeCell) {
+        this.up = tapeBlock;
+        tapeBlock.down = this;
+    }
+
+    joinDown(tapeBlock: TapeCell) {
+        this.down = tapeBlock;
+        tapeBlock.up = this;
+    }
+
+    toString(): string {
+        return this._symbol;
+    }
+}
