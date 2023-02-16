36f9dd2e74af3b77da423d78b95e5939bf75f9bc
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2023-02-16 15:33:04 +0800

    Create Direction.ts

---
+++ lib/utils/Direction.ts
@@ -0,0 +1,14 @@
+/**
+ * DO NOT MODIFY!
+ * This enum maps directly to the properties of TapeCell
+ * Modifying this file means modifying the properties defined in TapeCell.ts
+ */
+
+enum Directions {
+    LEFT = 'left',
+    RIGHT = 'right',
+    UP = 'up',
+    DOWN = 'down',
+}
+
+export default Directions;
